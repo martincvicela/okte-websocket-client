c590b9afeac5ba95ad2e350b30507860a4a8ead8
martincvicela <martin.cvicela@isdd.sk>
2025-05-19 19:50:35 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python okte-websocket-client.py --username $USERNAME --password $PASSWORD --client-cert cert.crt --client-key cert.pem --okte-ca okte_cert.pem --auto-save 60
+python okte-websocket-client.py --username $USERNAME --password $PASSWORD --client-cert cert.crt --client-key key.pem --okte-ca okte_cert.pem --auto-save 60
